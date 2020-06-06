561f52c2c5f7249b13b8193e186350fc138401ca
Shadorain <jordanf7517@gmail.com>
2020-06-06 12:31:24 -0400

    test

--- shadoplan.c
+++ shadoplan.c
@@ -14,7 +14,6 @@ struct Entry {
 void usage() {
     fputs("usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
             "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n", stderr);
-    exit(1);
 }
 void addDesc(Entry *e) {}
 
